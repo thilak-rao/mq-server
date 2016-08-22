990eeca68eedde0717b18dc70c12ec5f26c9e415
Thilak Rao <me@thilak.in>
2016-08-22 21:45:49 +0530

    Test were failing because task route and controller were not cleaned up in route.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,50 +1,9 @@
 import * as Hapi from "hapi";
-import TaskController from '../controllers/taskController';
 import UserController from '../controllers/user.controller';
-import TaskRepository from '../libs/repository/mongo/taskRepository';
 import UserRepository from '../libs/repository/mongo/userRepository';
 import {USER_API} from "../configs/CONSTANTS";
 
 export default function(server: Hapi.Server) {
-
-    const taskController = new TaskController(server, new TaskRepository());
-
-    server.route({
-        method: 'GET',
-        path: '/api/tasks/{id}',
-        handler: undefined,
-        config: taskController.getTaskById()
-    });
-
-    server.route({
-        method: 'GET',
-        path: '/api/tasks',
-        handler: undefined,
-        config: taskController.getTasks()
-    });
-
-    server.route({
-        method: 'DELETE',
-        path: '/api/tasks/{id}',
-        handler: undefined,
-        config: taskController.deleteTask()
-    });
-
-    server.route({
-        method: 'PUT',
-        path: '/api/tasks/{id}',
-        handler: undefined,
-        config: taskController.updateTask()
-    });
-
-    server.route({
-        method: 'POST',
-        path: '/api/tasks',
-        handler: undefined,
-        config: taskController.createTask()
-    });
-
-
 	// User API Routes
 	const userController = new UserController(server, new UserRepository());
 
